7b871636f1d166bf5fe2c918cba10cc6365b05c6
David Cesiel <dcesiel@gmail.com>
2013-04-04 21:47:08 -0400

    Added GunSubscriber and test of nerf gun

---
+++ robot/lcm/GunSubscriber.py
@@ -0,0 +1,41 @@
+import lcm
+from lcmtypes import drive_t
+from pyfirmata import Arduino, util
+
+DEBUG = False
+
+#Initialize Arduino board on ttyUSB0
+board = Arduino('/dev/ttyUSB0')
+print("GunSubscriber connected to Arduino")
+
+#Initialize servo PWM
+#This sets pin 10 to a digital pwm output
+pin10 = board.get_pin('d:10:p')
+
+#Initialize digital output to start gun
+pin13 = board.get_pin('d:13:o')
+
+def handler(channel, data):
+    msg = drive_t.decode(data)
+    if DEBUG:
+        print("   timestamp   = %s" % str(msg.timestamp))
+        print("   fire = %s" % str(msg.fire))
+
+    board.digital[13].write(1)
+    time.sleep(0.2)
+    pin10.write(175)
+    time.sleep(1.3)
+    pin10.write(23)
+    time.sleep(0.8)
+    board.digital[13].write(0)
+
+
+lc = lcm.LCM()
+subscription = lc.subscribe("GUN", handler)
+
+try:
+    while True:
+        lc.handle()
+        time.sleep(0.02);
+except KeyboardInterrupt:
+    pass
